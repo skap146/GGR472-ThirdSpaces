7f6a06fd6a5bf3da53ba39919c7600edb6214397
shawn kapcan <skapcan52@gmail.com>
2026-02-14 09:04:52 -0500

    added boiler-plate and a menu

---
+++ js/menu.js
@@ -0,0 +1,28 @@
+// menu pixel width when open
+const pixel_width = 250;
+
+// respond to menu button upon click
+const menu_btn = document.getElementById("menu_btn");
+
+menu_btn.addEventListener("click", ()  => {
+
+    let sidebar = document.getElementById("sidebar");
+    let curr_menu_width = sidebar.offsetWidth;
+
+    // main page element
+    let main = document.getElementById("main");
+
+    console.log(curr_menu_width);
+
+    // show menu if currently hidden, hide menu if currently displayed
+    if (curr_menu_width === 0)
+    {
+        sidebar.style.width = pixel_width + "px";
+        main.style.marginLeft = pixel_width + "px";
+    }
+    else
+    {
+        sidebar.style.width = 0 + "px";
+        main.style.marginLeft = 0 + "px";
+    }
+})

8036445f3327d6b1751de1ac7dab9a1cef88527b
shawn kapcan <skapcan52@gmail.com>
2026-04-13 10:06:46 -0400

    trying to fix buffers not functioning on orange points

--- js/third_place_map.js
+++ js/third_place_map.js
@@ -53,10 +53,10 @@ load_third_place_dropdown()
 // Add all third space elements to the dropdown menu
 function load_third_place_dropdown()
 {
-    load_layer_in_dropdown('../data/library.geojson', 'BranchName', 'library_point');
-    load_layer_in_dropdown('../data/EARLYONChildCentres.geojson', 'buildingName', 'early_child_centre_point');
-    load_layer_in_dropdown('../data/community_centres.geojson', 'ASSET_NAME', 'comm_centre_point');
-    load_layer_in_dropdown('../data/Places_of_Worship.geojson', 'PLACE_NAME', 'places_of_worship_point');
+    load_layer_in_dropdown('data/library.geojson', 'BranchName', 'library_point');
+    load_layer_in_dropdown('data/EARLYONChildCentres.geojson', 'buildingName', 'early_child_centre_point');
+    load_layer_in_dropdown('data/community_centres.geojson', 'ASSET_NAME', 'comm_centre_point');
+    load_layer_in_dropdown('data/Places_of_Worship.geojson', 'PLACE_NAME', 'places_of_worship_point');
 }
 
 // Loads the names for an individual specific that
@@ -153,10 +153,10 @@ const map = new mapboxgl.Map({
 map.on('load', () =>
 {
     // Load external GeoJSON files
-    map.addSource('library_data', {type: 'geojson',data: '../data/library.geojson'});
-    map.addSource('early_child_centre_data', {type: 'geojson', data: '../data/EarlyONChildCentres.geojson'});
-    map.addSource('comm_centre_data', {type: 'geojson', data: '../data/community_centres.geojson'});
-    map.addSource('places_of_worship_data', {type: 'geojson', data: '../data/Places_of_Worship.geojson'});
+    map.addSource('library_data', {type: 'geojson',data: 'data/library.geojson'});
+    map.addSource('early_child_centre_data', {type: 'geojson', data: 'data/EarlyONChildCentres.geojson'});
+    map.addSource('comm_centre_data', {type: 'geojson', data: 'data/community_centres.geojson'});
+    map.addSource('places_of_worship_data', {type: 'geojson', data: 'data/Places_of_Worship.geojson'});
 
     // Visualize building layer
     map.addLayer({
